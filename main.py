529797aa1811c5942f75eccead2c95a2b3bc3481
Mihai Ungureanu <32022907+ExplodingInsanity@users.noreply.github.com>
2021-11-11 13:12:58 +0200

    further improvements, "struggle" minigame should be flawless (?)

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import timeit
 from PIL import Image
 
 decisionThreshold = .8
-decisionThresholdCenter = .6
+decisionThresholdCenter = .8
 
 def resize(img,scale_percent):
     width = int(img.shape[1] * scale_percent / 100)
@@ -17,26 +17,41 @@ def resize(img,scale_percent):
 
 leftImg = cv2.imread('images/left_small.png');
 rightImg = cv2.imread('images/right_small.png');
-leftMiddleImg = cv2.imread('images/left_center_tip.png');
-rightMiddleImg = cv2.imread('images/right_center_tip.png');
+rightImgBig = cv2.imread('images/right_small.png');
+leftImgBig = cv2.imread('images/left_small.png');
+leftMiddleImg = cv2.imread('images/left_center_tip2.png');
+rightMiddleImg = cv2.imread('images/right_center_tip2.png');
 
-resize(leftImg,50)
-resize(rightImg,50)
-resize(leftMiddleImg,50)
-resize(rightMiddleImg,50)
+leftImg = cv2.cvtColor(leftImg,cv2.COLOR_RGB2GRAY)
+rightImg = cv2.cvtColor(rightImg,cv2.COLOR_RGB2GRAY)
+leftImgBig = cv2.cvtColor(leftImgBig,cv2.COLOR_RGB2GRAY)
+rightImgBig = cv2.cvtColor(rightImgBig,cv2.COLOR_RGB2GRAY)
+leftMiddleImg = cv2.cvtColor(leftMiddleImg,cv2.COLOR_RGB2GRAY)
+rightMiddleImg = cv2.cvtColor(rightMiddleImg,cv2.COLOR_RGB2GRAY)
+#leftMiddleImg = cv2.flip(rightMiddleImg,1)
+    
+# resize(leftImg,50)
+# resize(rightImg,50)
+rightImgBig = resize(rightImgBig,145)
+leftImgBig = resize(leftImgBig,145)
+# resize(rightMiddleImg,50)
 
 start_time = timeit.default_timer()
 
 def checkNormalHit():
     sct = mss()
-    im_bgr = np.array(sct.grab(monitor = {'top': 720, 'left': 600, 'width': 750, 'height': 200}))
+    im_bgr = np.array(sct.grab(monitor = {'top': 720, 'left': 400, 'width': 1100, 'height': 300}))
     im_bgr = np.flip(im_bgr[:, :, :3], 2)  # 1
-    im_bgr = cv2.cvtColor(im_bgr, cv2.COLOR_RGB2BGR)  # 2
+    im_bgr = cv2.cvtColor(im_bgr, cv2.COLOR_RGB2GRAY)  # 2
+    
+    # cv2.imshow('window',im_bgr)
+    # if cv2.waitKey(20) & 0xFF == ord('q'):
+    #     cv2.destroyAllWindows()
+
     # left
     result = cv2.matchTemplate(im_bgr,leftImg,cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    w = leftImg.shape[1]
-    h = leftImg.shape[0]
+    # print("left={}".format(max_val))
     if max_val > decisionThreshold:
         pyautogui.press('left')
         return True
@@ -44,18 +59,42 @@ def checkNormalHit():
     result = cv2.matchTemplate(im_bgr,rightImg,cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
-    w = rightImg.shape[1]
-    h = rightImg.shape[0]
     if max_val > decisionThreshold:
         pyautogui.press('right')
         return True
+    result = cv2.matchTemplate(im_bgr,rightImgBig,cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    # im_bgr = cv2.rectangle(im_bgr,max_loc,(max_loc[0]+rightImgBig.shape[1],max_loc[1]+rightImgBig.shape[0]),(255,255,255),2)
+    # cv2.imshow('window',im_bgr)
+    # if cv2.waitKey(20) & 0xFF == ord('q'):
+    #     cv2.destroyAllWindows()
+    # print("right={}".format(max_val))
+    if max_val > decisionThreshold:
+        pyautogui.press('right')
+        return True
+    result = cv2.matchTemplate(im_bgr,leftImgBig,cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+    # im_bgr = cv2.rectangle(im_bgr,max_loc,(max_loc[0]+leftImgBig.shape[1],max_loc[1]+leftImgBig.shape[0]),(255,255,255),2)
+    # cv2.imshow('window',im_bgr)
+    # if cv2.waitKey(20) & 0xFF == ord('q'):
+    #     cv2.destroyAllWindows()
+    # print(leftImgBig.shape)
+    # print("right={}".format(max_val))
+
+    if max_val > decisionThreshold:
+        pyautogui.press('left')
+        return True
+
     return False
     
 def checkMiddleHit():
     sct = mss()
-    im_bgr = np.array(sct.grab(monitor = {'top': 170, 'left': 800, 'width': 250, 'height': 310}))
+    im_bgr = np.array(sct.grab(monitor = {'top': 170, 'left': 750, 'width': 360, 'height': 310}))
     im_bgr = np.flip(im_bgr[:, :, :3], 2)  # 1
-    im_bgr = cv2.cvtColor(im_bgr, cv2.COLOR_RGB2BGR)  # 2
+    im_bgr = cv2.cvtColor(im_bgr, cv2.COLOR_RGB2GRAY)  # 2
+
+
 
     resize(im_bgr,50)
     # left
@@ -65,6 +104,11 @@ def checkMiddleHit():
     # right
     result = cv2.matchTemplate(im_bgr,rightMiddleImg,cv2.TM_CCOEFF_NORMED)
     min_val_right, max_val_right, min_loc_right, max_loc_right = cv2.minMaxLoc(result)
+    im_bgr = cv2.rectangle(im_bgr,max_loc_right,(max_loc_right[0]+rightMiddleImg.shape[1],max_loc_right[1]+rightMiddleImg.shape[0]),(255,0,0),2)
+    cv2.imshow('window',im_bgr)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+    # print("left={}, right={}".format(max_val_left,max_val_right))
 
     if max_val_right > decisionThresholdCenter and max_val_left > decisionThresholdCenter:
         pyautogui.press('left' if max_loc_left[1] > max_loc_right[1] else 'right')
